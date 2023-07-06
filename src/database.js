bd2ad924e62da2fbc515be270149a35e719a5aa4
tkosudi <tkosudi@gmail.com>
2023-07-06 15:43:37 -0300

    feat: add database.js

---
+++ src/database.js
@@ -0,0 +1,77 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url)
+
+export class Database {
+  #database = {}
+
+  constructor() {
+    fs.readFile(databasePath, 'utf-8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      }).catch(() => {
+        this.#persist()
+      })
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database))
+  }
+
+  select(table, search) {
+    let data  = this.#database[table] ?? []
+
+    if (search) {
+      console.log(search)
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          console.log('row[key] => ', row[key])
+          console.log('value => ', value)
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+
+    return data
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+
+    this.#persist()
+
+    return data
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+    
+    if (rowIndex > -1) {
+      const originalRow = this.#database[table][rowIndex];
+      const updatedRow = {
+        id: originalRow.id,
+        title: data.title ?? originalRow.title,
+        description: data.description ?? originalRow.description,
+        completed_at: originalRow.completed_at,
+        created_at: originalRow.created_at,
+        updated_at: new Date().toISOString()
+      };
+  
+      this.#database[table][rowIndex] = updatedRow;
+      this.#persist()
+    }
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
+}
